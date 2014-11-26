0de4991e0579c8983f987df704cdcc644d701eff
mjahnen <jahnen@in.tum.de>
2014-11-26 18:42:11 +0100

    The java SCS can now generate TANs from a file

--- java/SmartCardSim/src/scs/Controller.java
+++ java/SmartCardSim/src/scs/Controller.java
@@ -10,7 +10,9 @@ import javafx.scene.control.TextField;
 import javafx.scene.input.Clipboard;
 import javafx.scene.input.ClipboardContent;
 import javafx.scene.paint.Color;
+import javafx.stage.FileChooser;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.security.MessageDigest;
@@ -28,11 +30,14 @@ public class Controller implements Initializable {
     @FXML
     private TextField amountTextField;
     @FXML
+    private Label fileLabel;
+    @FXML
     private Button copyTANButton;
     @FXML
     private TabPane tabPane;
 
-    String tan;
+    private String tan;
+    private File file;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -50,6 +55,14 @@ public class Controller implements Initializable {
     }
 
     @FXML
+    private void searchClicked(ActionEvent event) {
+        FileChooser chooser = new FileChooser();
+        file = chooser.showOpenDialog(statusLabel.getScene().getWindow());
+
+        fileLabel.setText("File: " + file.getName());
+    }
+
+    @FXML
     private void generateTANClicked(ActionEvent event) {
         statusLabel.setVisible(false);
         copyTANButton.setVisible(false);
@@ -58,9 +71,40 @@ public class Controller implements Initializable {
             return;
         }
 
+        String destination = null;
+        String amount = null;
+
+        if(tabPane.getSelectionModel().getSelectedIndex() == 0) {
+            destination = destinationTextField.getText();
+            amount = amountTextField.getText();
+        } else {
+            FileParser parser = new FileParser(file);
+            try {
+                parser.parse();
+                destination = parser.getDestination();
+                amount = parser.getAmount();
+
+                if(destination == null || amount == null) {
+                    setNegativeStatus("Destination or amount missing in file!");
+                    return;
+                }
+
+                if(!validateInputs(destination, amount)) {
+                    return;
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+                setNegativeStatus("Cannot read from file!");
+                return;
+            }
+        }
+
+        generateTAN(destination, amount);
+    }
+
+    private void generateTAN(String destination, String amount) {
         String pin = pinTextField.getText();
-        String destination = destinationTextField.getText();
-        String amount = amountTextField.getText();
 
         long time;
         try {
@@ -103,11 +147,6 @@ public class Controller implements Initializable {
         String destination = destinationTextField.getText();
         String amount = amountTextField.getText();
 
-        if(pin.isEmpty() || destination.isEmpty() || amount.isEmpty()) {
-            setNegativeStatus("Please fill in all fields!");
-            return false;
-        }
-
         if(pin.length() != 6) {
             setNegativeStatus("PIN must have exactly six digits!");
             return false;
@@ -115,6 +154,22 @@ public class Controller implements Initializable {
 
         if(!pin.matches("[0-9]+")) {
             setNegativeStatus("PIN must contain only digits!");
+            return false;
+        }
+
+        // If we are using a batch file just test the file
+        if(tabPane.getSelectionModel().getSelectedIndex() == 1) {
+            setNegativeStatus("Please specify a file!");
+            return file != null;
+        }
+
+        return validateInputs(destination, amount);
+    }
+
+    private boolean validateInputs(String destination, String amount) {
+
+        if(destination.isEmpty() || amount.isEmpty()) {
+            setNegativeStatus("Please fill in all fields!");
             return false;
         }
 
